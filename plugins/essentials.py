7381095a76a18e132a0ec3926dbc5902ca14a845
Julien00859 <Julien00859@gmail.com>
2015-12-17 01:28:52 +0100

    azeazeaze

--- plugins/essentials.py
+++ plugins/essentials.py
@@ -1,8 +1,52 @@
-def on_public_message(api, channel, sender, message):
-    if message == "!stop":
-        api.stop()
-    elif message == "!ping":
-        api.send_message(channel, "Pong!")
+import urllib.request
+from bs4 import BeautifulSoup
+from sched import scheduler
+from threading import Thread
+import os.path
+from os import getcwd
+import random
+
+API = None # Variable global
+
+"""Fonctions périodiques"""
+def period_thread(interval, function):
+    # Lance le scheduler dans un nouveau thread
+    Thread(target=period_sched, args=(interval, function), daemon=True).start()
+def period_sched(interval, function):
+    # Exécute la fonction et schedule la prochaine
+    function()
+    s = scheduler()
+    s.enter(interval, 1, period_thread)
+    s.run()
+
+
+def on_public_message(channel, sender, message):
+    if message == "!ping":
+        API.send_message(channel, "Pong!")
+
+    elif message == "!quote":
+        API.send_message(channel, random.choice(open(os.path.join(getcwd(), "plugins", "essentials", "eliot.txt"), "r").readlines()))
+
+
+def on_private_message(sender, message):
+    args = message.split(" ")
+    if len(args) >= 2 and args[1] == "stop":
+        API.stop("We live in a kingdom of bullshit.")
+
 
 def on_stop():
     pass
+
+
+def on_load(api):
+    global API
+    API = api
+    period_thread(60*60*24, quote)
+
+
+def quote():
+    soup = BeautifulSoup(urllib.request.urlopen("http://www.softwarequotes.com/").read(), "html.parser")
+    qotd = soup.find(id="QuoteoftheDay").getText().replace("\n", "")
+    author = soup.find(id="Span1").getText().replace("\n", "")[2:]
+    for channel in API.get_channels():
+        API.send_command("TOPIC {} {} ~{}".format(channel, qotd, author))
--- plugins/skype.py
+++ plugins/skype.py
@@ -1,25 +1,31 @@
 import subprocess
 import threading
 
+API = None
+
 def read():
-	lines = proc.stdout.readline().decode()
-	for line in lines.split("\n"):
-		if len(line) == 0: continue
-		print(line)
-		api.sendMessage(api.getChannels()[0], line)
+    lines = proc.stdout.readline().decode()
+    for line in lines.split("\n"):
+        if len(line) == 0: continue
+        print(line)
+        API.sendMessage(API.getChannels()[0], line)
 
-def onPublicMessage(api, channel, sender, message):
-	proc.stdin.write("sk.write('{}','{}')\r\n".format(sender, message).encode())
 
-def onLoop(api):
-	proc.stdin.write("sk.read()\r\n".encode())
+def on_public_message(channel, sender, message):
+    proc.stdin.write("sk.write('{}','{}')\r\n".format(sender, message).encode())
 
-def onLoad(api):
-	captureThread = threading.Thread(target=read, args=())
-	captureThread.daemon = True
-	captureThread.start()
 
-def onStop():
-	proc.kill()
+def on_loop():
+    proc.stdin.write("sk.read()\r\n".encode())
 
-proc = subprocess.Popen("F:\\Python27\\python.exe plugins\\skype\\wrapper.py", stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def on_load(api):
+    global API
+    API = api
+    # threading.Thread(target=read, args=(api), daemon=True).start()
+
+
+def on_stop():
+    proc.kill()
+
+proc = subprocess.Popen("F:\\Python27\\python.exe plugins\\skype\\wrapper.py", stdin=subprocess.PIPE)
