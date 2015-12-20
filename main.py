948cc681368b1e56e8d6a527c534a847eca938a2
Julien00859 <Julien00859@gmail.com>
2015-12-20 02:33:30 +0100

    Déplacement des classes Plugin et API dans d'autres fichiers, gestion dynamique des modes, ajout du sender pour l'event on_mode

--- main.py
+++ main.py
@@ -1,132 +1,10 @@
 #!/usr/bin/python3.5
-# -*- coding: utf-8 -*-
 
-from importlib import import_module
-from os import getcwd
 from select import select
 import json
-import os.path
 import socket
-
-
-class Plugins:
-    def __init__(self, api):
-        """
-        Gestionnaire de plugin
-        Importe les plugins python écrits dans /plugins/
-        :param api: Un objet API() pour permettre aux plugins de profiter de ses méthodes
-        """
-        self.API = api
-
-        plugins_path = os.path.join(getcwd(), "plugins")
-        if not os.path.exists(plugins_path):
-            os.mkdir("plugins")
-
-        self.plugins = []
-        for plugin in os.listdir(plugins_path):
-            if plugin.endswith(".py") and plugin != "__init__.py":
-                self.plugins.append(import_module("plugins." + plugin[:plugin.find(".")]))
-
-    def load(self) -> None:
-        for plugin in self.plugins:
-            if "on_load" in plugin.__dict__:
-                plugin.on_load(self.API)
-
-    def loop(self) -> None:
-        for plugin in self.plugins:
-            if "on_loop" in plugin.__dict__:
-                plugin.on_loop()
-
-    def private_message(self, sender: str, message: str) -> None:
-        for plugin in self.plugins:
-            plugin_name = plugin.__name__[plugin.__name__.find(".")+1:].casefold()
-            message_plugin = message.split(" ")[0].casefold()
-            if plugin_name == message_plugin and "on_private_message" in plugin.__dict__:
-                plugin.on_private_message(sender, message)
-                break
-
-    def public_message(self, channel: str, sender: str, message: str) -> None:
-        for plugin in self.plugins:
-            if "on_public_message" in plugin.__dict__:
-                plugin.on_public_message(channel, sender, message)
-
-    def join(self, channel: str, user: str) -> None:
-        for plugin in self.plugins:
-            if "on_join" in plugin.__dict__:
-                plugin.on_join(channel, user)
-
-    def stop(self) -> None:
-        print("Stopping...")
-        for plugin in self.plugins:
-            plugin.on_stop()
-
-
-class API:
-    def __init__(self, server):
-        self._server = server
-        self._config = json.load(open("config.json", "r"))
-        self._channels = {}
-
-    def stop(self, message="Stopped") -> None:
-        self._server.plugins.stop()
-        self.send_command("QUIT :" + message)
-        self._server.running = False
-
-    def send_command(self, cmd: str) -> None:
-        self._server.socket.send("{}\r\n".format(cmd).encode())
-
-    def send_message(self, channel: str, message: str) -> None:
-        self.send_command("PRIVMSG {} :{}\r\n".format(channel, message))
-
-    def get_bot_nick(self) -> str:
-        return self._config["nickname"]
-
-    def get_channels(self) -> list:
-        return self._config["channels"]
-
-    def add_update_user(self, channel: str, user: str) -> None:
-        if user:
-            if channel not in self._channels:
-                self._channels[channel] = {}
-
-            modes = {"@":("OP", 4), "%":("HALF-OF", 3), "+":("VOICE", 2)}
-            if user[:1] in modes:
-                if user not in self._channels[channel]:
-                    self._channels[channel][user[1:]] = {}
-                self._channels[channel][user[1:]]["permission"] = modes[user[:1]]
-            else:
-                if user not in self._channels[channel]:
-                    self._channels[channel][user] = {}
-                self._channels[channel][user]["permission"] = ("USER",1)
-
-    def remove_user(self, channel: str, user: str) -> None:
-        if channel in self._channels:
-            if user in self._channels[channel]:
-                del self._channels[channel][user]
-            else:
-                raise ValueError("User \"{}\" not found".format(user))
-        else:
-            raise ValueError("Channel \"{}\" not found".format(channel))
-
-    def get_user_permissions(self, user: str, channel: str=None) -> tuple:
-        # Si un channel est spécifié, retourner le niveau de permission relatif à ce salon
-        if channel in self._channels:
-            if user in self._channels[channel]:
-                return self._channels[channel][user]["permission"]
-            else:
-                return "DISCONNECTED", 0
-
-        # Sinon retourner le plus haut niveau de permission relatif à l'utilisateur sur tous les salons
-        elif channel is None:
-            perm = ("USER",0)
-            for channel in self.get_channels():
-                mode, lvl = self.get_user_permissions(user, channel)
-                if perm[1] < lvl:
-                    perm = (mode, lvl)
-            return perm
-
-        else:
-            raise ValueError("Channel \"{}\" not found".format(channel))
+from API import API
+from Plugins import Plugins
 
 
 class Server:
@@ -142,8 +20,6 @@ class Server:
             sleep_time = config["sleepTime"]
             nick = config["nickname"]
             oper_password = config["oper_password"]
-
-        commands = {"PING": self.ping, "PRIVMSG": self.privmsg, "JOIN": self.join, "KICK": self.kick, "PART": self.part, "MODE": self.mode, "QUIT": self.quit}
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host, port))
@@ -161,19 +37,20 @@ class Server:
 
         for channel in self.API.get_channels():
             self.API.send_command("JOIN " + channel)
-
-            #Prend la main pour lire le /NAMES lié au /join
-            names = self.socket.recv(1024).decode().split("\r\n")
-            for n in range(len(names)):
-                if "End of /NAMES list." in names[n]:
-                    # :127.0.0.1 353 Julien008 = #Dev :@Julien008 MrRobot
-                    #                                 [@Julien008,MrRobot]
-                    for user in " ".join(names[n-1].split(" ")[5:])[1:].split(" "):
-                        self.API.add_update_user(channel, user)
-
             self.API.send_command("SAMode {} +o {}".format(channel, nick))
+            self.API.send_names_query(channel)
 
         self.plugins.load()
+
+        commands = {
+            "PING": self.ping,
+            "PRIVMSG": self.privmsg,
+            "JOIN": self.join,
+            "KICK": self.kick,
+            "PART": self.part,
+            "MODE": self.mode,
+            "QUIT": self.quit
+        }
         self.running = True
         while self.running:
             rlist, wlist, xlist = select([self.socket], [], [], sleep_time)
@@ -197,6 +74,8 @@ class Server:
 
             self.plugins.loop()
 
+        self.socket.close()
+
     def ping(self, args: list) -> None:
         # PING :127.0.0.1
         if len(args) >= 2:
@@ -206,8 +85,8 @@ class Server:
         if len(args) >= 4:
             # :Julien00859!Julien@host-85-201-171-39.dynamic.voo.be PRIVMSG #Dev :plop
             #  ^^^^^^^^^^^                                                  ^^^^
-            sender = args[0][1:args[0].find("!")] # Julien00859
-            channel = args[2] # #Dev
+            sender = args[0][1:args[0].find("!")]  # Julien00859
+            channel = args[2]  # #Dev
             if channel in self.API.get_channels():
                 self.plugins.public_message(channel, sender, " ".join(args[3:len(args)])[1:])
 
@@ -218,10 +97,10 @@ class Server:
         if len(args) >= 3:
             # :Julien00859!Julien@host-85-201-171-39.dynamic.voo.be JOIN :#Dev
             #  ^^^^^^^^^^^                                                ^^^^
-            sender = args[0][1:args[0].find("!")] # Julien00859
-            channel = args[2][1:] # #Dev
-            self.plugins.join(channel, sender)
+            sender = args[0][1:args[0].find("!")]  # Julien00859
+            channel = args[2][1:]  # #Dev
             self.API.add_update_user(channel, sender)
+            self.plugins.join(channel, sender)
 
     def kick(self, args: list) -> None:
         # :Julien008!Julien@host-85-201-171-39.dynamic.voo.be KICK #Dev Julien00859 :Julien008
@@ -233,34 +112,25 @@ class Server:
         if len(args) >= 3:
             # :Julien00859!Julien@host-85-201-171-39.dynamic.voo.be PART #Dev :"Gonna bake some cookies..."
             #  ^^^^^^^^^^^                                               ^^^^
-            sender = args[0][1:args[0].find("!")] # Julien00859
+            sender = args[0][1:args[0].find("!")]  # Julien00859
             self.API.remove_user(args[2], sender)
 
     def mode(self, args: list) -> None:
         # :Julien008!Julien@host-85-201-171-39.dynamic.voo.be MODE #Dev +h Julien00859
-        #                                                          ^^^^    ^^^^^^^^^^^
+        #  ^^^^^^^^^                                               ^^^^ ^^ ^^^^^^^^^^^
 
-        # Reprend la main sur le socket pour envoyer un /NAMES sur le channel concerné par la commande et analyser son retour
+        sender = args[0][1:args[0].find("!")]  # Julien008
         if len(args) >= 5:
-            channel = args[2]
-            user = args[4]
-            self.API.send_command("NAMES " + channel)
-            names = self.socket.recv(1024).decode().split("\r\n")
-            for n in range(len(names)):
-                if "End of /NAMES list." in names[n]:
-                    # :127.0.0.1 353 Julien008 = #Dev :@Julien008 MrRobot
-                    #                                 [@Julien008,MrRobot]
-                    for user in " ".join(names[n-1].split(" ")[5:])[1:].split(" "):
-                        self.API.add_update_user(channel, user)
+            self.API.send_names_query(args[2])
+            self.plugins.mode(sender, args[2], args[3], args[4])
 
     def quit(self, args: list) -> None:
         if len(args) >= 1:
             # :Julien00859!Julien@host-85-201-171-39.dynamic.voo.be QUIT :Quit: Keep calm and do not rage quit
             #  ^^^^^^^^^^^
-            sender = args[0][1:args[0].find("!")] # Julien00859
+            sender = args[0][1:args[0].find("!")]  # Julien00859
             for channel in self.API.get_channels():
                 self.API.remove_user(channel, sender)
-
 
 if __name__ == "__main__":
     Server()
