c246d10479bac852626a8e191cb351d0eedfe8f5
Julien00859 <Julien.castiaux@gmail.com>
2015-12-31 03:56:08 +0100

    Debug

--- plugins/auth.py
+++ plugins/auth.py
@@ -12,7 +12,7 @@ def on_load(api):
     API = api
 
     if "auth.json" in listdir(join(getcwd(), "plugins")):
-        auth = json.load(open(join(getcwd(), "plugins", "auth.json")), "r")
+        auth = json.load(open(join(getcwd(), "plugins", "auth.json"), "r"))
     else:
         auth = {}
 
@@ -25,9 +25,8 @@ def on_load(api):
                 auth[user]["authenticated"] = False
                 auth[user]["channels"] = {}
                 auth[user]["channels"][channel] = API.get_user_mode(user, channel)[0]
-            else:
-                # Utilisateur gelé, on ne le modifie pas
-                pass
+            elif "password" in auth[user]:
+                auth[user]["authenticated"] = False
 
 
 def on_stop():
@@ -49,9 +48,9 @@ def on_private_message(sender, message):
                         # L'authentification est réussie
                         # Maintenant on envoie un /MODE dans tous les salons où il se trouve
                         auth[sender]["authenticated"] = True
-                        for channel in API.get_channels:
+                        for channel in API.get_channels():
                             if sender in API.get_users(channel) and channel in auth[sender]["channels"]:
-                                API.set_user_mode(channel, auth[sender][channel], sender)
+                                API.set_user_mode(channel, auth[sender]["channels"][channel], sender)
                         API.send_message(sender, "[AUTH] Authentication successful.")
                     else:
                         API.send_message(sender, "[AUTH] Invalid password !")
@@ -72,6 +71,7 @@ def on_private_message(sender, message):
                     auth[sender]["authenticated"] = True
                     API.send_message(sender, "[AUTH] You have been registered.")
                 else:
+                    auth[sender]["authenticated"] = False
                     API.send_message(sender, "[AUTH] You are already registered ! Please type: \"/msg {} auth login {}\" to login with your account".format(API.get_bot_nick(), args[2]))
             else:
                 API.send_message(sender, "[AUTH] Usage: \"/msg {} register a_password\"".format(API.get_bot_nick()))
@@ -134,5 +134,5 @@ def on_mode(sender, channel, mode, user):
 
 
 def on_quit(sender):
-    if sender in auth and not auth[sender]["authenticated"]:
+    if sender in auth and "authenticated" in auth[sender] and auth[sender]["authenticated"]:
         auth[sender]["authenticated"] = False
